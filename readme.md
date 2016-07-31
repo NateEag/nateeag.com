446883fad7b883b49286ac77e5ed431d7ce17a62
Nate Eagleson <nate@nateeag.com>
2016-07-31 08:12:12 -0400

    watch.sh re-renders site when a file in pages/ changes
    This way I won't have to run generate.sh by hand all the time.

--- readme.md
+++ readme.md
@@ -3,7 +3,8 @@ nateeag.com
 
 This is the source code and content for www.nateeag.com.
 
-It's statically generated using a few Python modules and a little bit of code.
+It's statically generated using a few Python modules and a small amount of
+wrapper code.
 
 Usage
 =====
@@ -19,3 +20,6 @@ To view the generated code, just view it through a browser. An easy way:
     $ python -m SimpleHTTPServer
 
 then open a browser and hit localhost:8000.
+
+`watch.sh` uses the [fswatch](https://github.com/emcrisostomo/fswatch) command
+to re-render the site when something in the `pages/` directory changes.
---
+++ watch.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+# Watch the content folder and re-render the site when it changes.
+
+# Depends on the fswatch program. Mostly exists so I don't have to remember
+# fswatch syntax.
+
+project_path=$(dirname "$0")
+
+. "$project_path/virtualenv/bin/activate"
+
+# The xargs invocation is a bit labored in order to output the timestamp of
+# each re-rendering, and xargs doesn't have long option names, so some
+# explanation is due.
+#
+# We execute a new bash instance for each line of input, then run the specified
+# command in it.
+#
+# The first part of the command is double-quoted to interpolate the path to the
+# script safely in the face of whitespace, while the second half is defined as
+# a variable, so that it will not be interpolated by the current shell, which
+# would result in the same timestamp for all items.
+#
+# Instead, it is interpolated by the new bash instance that is spawned by the
+# new line of output from fswatch, and therefore gets a
+# roughly-accurate timestamp.
+
+OUTPUT_DATE_CMD='echo $(date "+%Y-%m-%d %H:%M:%S"): Rendered site'
+
+fswatch --one-per-batch "$project_path/pages" | \
+    xargs -L 1 bash -c "$project_path/generate.sh && $OUTPUT_DATE_CMD"
